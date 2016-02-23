f04f42b62f106a7861b9fd9593c95dc0ee5ccff9
sobolevn <mail@sobolevn.me>
2016-02-24 00:28:45 +0300

    improve gh-pages target

--- utils/gh-branch.sh
+++ utils/gh-branch.sh
@@ -7,8 +7,9 @@ function update_gh_branch {
   git checkout gh-pages
   make
 
-  git add --all
+  git add _posts
   git commit -m 'documentation update'
+  git checkout master
 }
 
 update_gh_branch
