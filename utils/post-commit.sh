1904ebdb86092f9c67674a0b6f34d600993d9c03
mail@sobolevn.me <ellina.morits@gmail.com>
2016-03-13 14:07:30 +0300

    pre-staging

--- utils/post-commit.sh
+++ utils/post-commit.sh
@@ -4,15 +4,16 @@ set -e
 
 BRANCH_NAME=$(git branch | grep '*' | sed 's/* //')
 
-if [[ "$BRANCH_NAME" == 'master' ]]; then
+if [[ "$BRANCH_NAME" == 'develop' ]]; then
   # Build new web documentation:
   make build-gh-pages
 fi
 
-if [[ "$BRANCH_NAME" == 'develop' ]]; then
+if [[ "$BRANCH_NAME" == 'staging' ]]; then
   # create new release:
   NEWEST_TAG=$(git describe --abbrev=0 --tags)
   SCRIPT_VERSION=$(bash ${PWD}/git-secret --version)
   if [[ "$NEWEST_TAG" != "v${SCRIPT_VERSION}" ]]; then
-      git tag -a "v${SCRIPT_VERSION}" -m "version $SCRIPT_VERSION"
+    git tag -a "v${SCRIPT_VERSION}" -m "version $SCRIPT_VERSION"
+  fi
 fi
