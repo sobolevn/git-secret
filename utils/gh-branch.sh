38983de6ee22de88b126c9c2637e76b832ae7a85
mail@sobolevn.me <ellina.morits@gmail.com>
2016-03-13 14:09:47 +0300

    staging

--- utils/gh-branch.sh
+++ utils/gh-branch.sh
@@ -4,12 +4,13 @@ set -e
 
 
 function update_gh_branch {
+  local branch_name=$(git branch | grep '*' | sed 's/* //')
   git checkout gh-pages
   make
 
   git add _posts
   git commit -m 'documentation update'
-  git checkout master
+  git checkout "$branch_name"
 }
 
 update_gh_branch
