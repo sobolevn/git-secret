874a4468d78ac4669ec337a949f799ff14fab0aa
mail@sobolevn.me <ellina.morits@gmail.com>
2016-03-13 13:27:36 +0300

    release

--- utils/post-commit.sh
+++ utils/post-commit.sh
@@ -7,11 +7,12 @@ BRANCH_NAME=$(git branch | grep '*' | sed 's/* //')
 if [[ "$BRANCH_NAME" == 'master' ]]; then
   # Build new web documentation:
   make build-gh-pages
+fi
 
+if [[ "$BRANCH_NAME" == 'develop' ]]; then
   # create new release:
   NEWEST_TAG=$(git describe --abbrev=0 --tags)
   SCRIPT_VERSION=$(bash ${PWD}/git-secret --version)
-  if [[ "$NEWEST_TAG" != "$SCRIPT_VERSION" ]]; then
-      git tag -a "$SCRIPT_VERSION" -m "version $SCRIPT_VERSION"
-  fi
+  if [[ "$NEWEST_TAG" != "v${SCRIPT_VERSION}" ]]; then
+      git tag -a "v${SCRIPT_VERSION}" -m "version $SCRIPT_VERSION"
 fi
