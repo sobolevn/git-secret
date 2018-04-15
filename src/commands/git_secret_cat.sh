14490649e83aa55ed1450d3da6de6a36199fcf3f
Josh Rabinowitz <joshr@joshr.com>
2018-04-15 16:10:00 -0400

    error if asked to decompress an unknown filename

--- src/commands/git_secret_cat.sh
+++ src/commands/git_secret_cat.sh
@@ -25,6 +25,8 @@ function cat {
   _user_required
 
   # Command logic:
+  local fsdb
+  fsdb=$(_get_secrets_dir_paths_mapping)
 
   for line in "$@"
   do
@@ -33,6 +35,13 @@ function cat {
 
     filename=$(_get_record_filename "$line")
     path=$(_append_root_path "$filename")
+    key=$(_get_encrypted_filename "$path")
+
+    local already_in
+    already_in=$(_fsdb_has_record "$key" "$fsdb")
+    if [[ "$already_in" -ne 1 ]]; then
+      _abort "unknown file: $path"
+    fi
 
     # The parameters are: filename, write-to-file, force, homedir, passphrase
     _decrypt "$path" "0" "0" "$homedir" "$passphrase"
