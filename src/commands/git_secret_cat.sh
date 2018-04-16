3f96947d88d50448a2cdb04a0140306a8b636db4
Josh Rabinowitz <joshr@joshr.com>
2018-04-16 09:16:40 -0400

    improve error checking

--- src/commands/git_secret_cat.sh
+++ src/commands/git_secret_cat.sh
@@ -25,21 +25,25 @@ function cat {
   _user_required
 
   # Command logic:
-  local fsdb
-  fsdb=$(_get_secrets_dir_paths_mapping)
+  #local fsdb
+  #fsdb=$(_get_secrets_dir_paths_mapping)
 
-  for line in "$@"
+  for item in "$@"
   do
-    local filename
-    local path
+    local path  # absolute path
+    local normalized_path # relative to the .git dir
+    normalized_path=$(_git_normalize_filename "$item")
+    path=$(_append_root_path "$normalized_path")
 
-    filename=$(_get_record_filename "$line")
-    path=$(_append_root_path "$filename")
+    local ignored
+    ignored=$(_check_ignore "$path")
 
-    local already_in
-    already_in=$(_fsdb_has_record "$filename" "$fsdb")
-    if [[ "$already_in" -eq 1 ]]; then
-      _abort "unknown filename: $filename (path $path)"
+    # Checking that file is valid:
+    if [[ ! -f "$path" ]]; then
+      _abort "$item is not a file."
+    elif [[ "$ignored" -ne 0 ]]; then
+      # abort on un-ignored files
+      _abort "$path is not an ignored file."
     else
       # The parameters are: filename, write-to-file, force, homedir, passphrase
       _decrypt "$path" "0" "0" "$homedir" "$passphrase"
