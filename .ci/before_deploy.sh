3fbec99dccb81b887e20c281bcef6873429e6fb3
simbo1905 <simbo1905@60hertz.com>
2018-06-07 14:56:54 +0100

    debug output in .ci/before_deploy.sh

--- .ci/before_deploy.sh
+++ .ci/before_deploy.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+echo "£££ before deploy with GITSECRET_DIST=${GITSECRET_DIST}, GITSECRET_DIST=${GITSECRET_DIST}, KITCHEN_REGEXP=${KITCHEN_REGEXP}"
+
+echo "£££ before deploy with GITSECRET_DIST=${GITSECRET_DIST}"
+
 if [[ "$GITSECRET_DIST" == "rpm" ]]; then
+  echo "£££ sudo apt-get install -y rpm;"
   # To deploy `rpm`-packages this utility is needed:
   sudo apt-get install -y rpm;
 fi
 
 if [[ ! -z "$GITSECRET_DIST" ]] && [[ -z "$KITCHEN_REGEXP" ]]; then
+  echo "£££ sudo apt-get install -y rpm;"
   # When making a non-container build, this step will generate
   # proper manifest files:
   make "deploy-$GITSECRET_DIST";
