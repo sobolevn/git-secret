73c81747ccaa42a526f2df774aadeeae1868f5bd
Josh Rabinowitz <joshr@joshr.com>
2018-06-13 14:03:30 -0400

    fix indentation

--- src/commands/git_secret_killperson.sh
+++ src/commands/git_secret_killperson.sh
@@ -31,10 +31,10 @@ function killperson {
 
   for email in "${emails[@]}"; do
     $gpg_local --batch --yes --delete-key "$email"
-      local exit_code=$?
-      if [[ "$exit_code" -ne 0 ]]; then
-        _abort "problem deleting key with gpg: exit code $exit_code"
-      fi
+    local exit_code=$?
+    if [[ "$exit_code" -ne 0 ]]; then
+      _abort "problem deleting key with gpg: exit code $exit_code"
+    fi
   done
 
   echo 'removed keys.'
