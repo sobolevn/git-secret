711a6889c3c32cc2b25e537e7d80f5548962a11b
Josh Rabinowitz <joshr@joshr.com>
2019-01-11 10:14:22 -0500

    travis-ci changes: remove ansible v2.3, fix brew build issue
    * remove ansible avm v2.3 pip install
    * work around perlbrew/build issue #305 with 'brew update' before installs

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -11,13 +11,14 @@ function update_linux() {
 function install_ansible {
   bash .ci/ansible-setup.sh
   # pyOpen, ndg-* and pyasn1 are for 'InsecurePlatformWarning' error
-  ~/.avm/v2.3/venv/bin/pip install netaddr ansible-lint   pyOpenSSL ndg-httpsclient pyasn1
   ~/.avm/v2.5/venv/bin/pip install netaddr ansible-lint   pyOpenSSL ndg-httpsclient pyasn1
 }
 
 
 # Mac:
 if [[ "$GITSECRET_DIST" == "brew" ]]; then
+  brew update 
+
   gnupg_installed="$(brew list | grep -c "gnupg")"
   [[ "$gnupg_installed" -ge 1 ]] || brew install gnupg
   if [[ -f "/usr/local/bin/gpg1" ]]; then
