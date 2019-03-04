fcf9d09bf67c1835b1bd175b560a5cf0b6bff3bf
Jonas Karlsson <jonas@y3k.se>
2019-03-04 16:26:05 +0100

    Add qoutes to prevent word splitting when passing $OPTARG to _clean_path function

--- src/commands/git_secret_tell.sh
+++ src/commands/git_secret_tell.sh
@@ -35,7 +35,7 @@ function tell {
 
       m) self_email=1;;
 
-      d) homedir=$(_clean_path $OPTARG);;
+      d) homedir=$(_clean_path "$OPTARG");;
 
       *) _invalid_option_for 'tell';;
     esac
