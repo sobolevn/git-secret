b41c5161c421bb72dffa9d90f0bba6fe613f51d4
Jonas Karlsson <jonas@y3k.se>
2019-03-07 14:50:46 +0100

    Revert "Enable debug output for 'tell' in test setup"
    This reverts commit d82b40cb0b942f45baa2bd133d906d0b3b75890e.

--- tests/_test_base.bash
+++ tests/_test_base.bash
@@ -222,8 +222,7 @@ function set_state_secret_tell {
   local email
 
   email="$1"
-  git secret tell -v -d "$TEST_GPG_HOMEDIR" "$email"
-  # > /dev/null 2>&1
+  git secret tell -d "$TEST_GPG_HOMEDIR" "$email" > /dev/null 2>&1
 }
 
 
