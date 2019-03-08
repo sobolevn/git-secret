6cbae4eece49748e194efa8ce043fbc835e7df08
Josh Rabinowitz <joshr@joshr.com>
2019-03-07 22:14:56 -0500

    only show diagnostic output when there is a pid

--- tests/_test_base.bash
+++ tests/_test_base.bash
@@ -69,9 +69,12 @@ function stop_gpg_agent {
   local pid
   pid=$(pgrep -U "$username" -x gpg-agent)
   # shellcheck disable=SC2001
-  echo "$pid" | sed "s/^/# '$BATS_TEST_DESCRIPTION' gpg-agent pid: /" >&3
 
   if [[ -n "$pid" ]] && [ "$pid" -gt 0 ]; then
+
+    #diagnostic output
+    echo "$pid" | sed "s/^/# '$BATS_TEST_DESCRIPTION' gpg-agent pid: /" >&3
+
     kill "$pid"
   fi
 }
