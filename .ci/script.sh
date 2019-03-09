c95b33ee39233e97325b3d6b098d98127d66cca4
Josh Rabinowitz <joshr@joshr.com>
2019-03-09 11:26:04 -0500

    don't run 'make lint' on windows yet

--- .ci/script.sh
+++ .ci/script.sh
@@ -12,11 +12,16 @@ function run_kitchen_tests {
   bundle exec kitchen test --test-base-path="$PWD/.ci/integration" $KITCHEN_REGEXP
 }
 
+# Only running `make test` on standard (non-docker) builds,
+# since it is called inside the docker container anyway.
+
 # Local builds:
-if [[ "$GITSECRET_DIST" == "brew" ]] || [[ "$GITSECRET_DIST" == "windows" ]]; then
-  # Only running `make test` on standard (non-docker) build,
-  # since it is called inside the docker container anyway.
+if [[ "$GITSECRET_DIST" == "brew" ]]; then
   make lint
+  make test
+fi
+if [[ "$GITSECRET_DIST" == "windows" ]]; then
+  #make lint
   make test
 fi
 
