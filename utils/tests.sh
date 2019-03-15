36892264a52c9920f0f22bfebb53457f7115d186
Jonas Karlsson <jonas@y3k.se>
2019-03-15 22:49:47 +0100

    Don't make use of a "run" directory from tests.sh as we have that for each test now

--- utils/tests.sh
+++ utils/tests.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
 set -e
+
 # `SECRET_PROJECT_ROOT` must be set before running the script.
 [ -z "${SECRET_PROJECT_ROOT}" ] && exit 1
 
+trap cleanup EXIT
+
 function cleanup {
-    rm -rf "${TEST_RUN_DIR}" "${TEST_TMP_DIR}"
+    rm -rf "${TEST_TMP_DIR}"
     # shellcheck disable=SC2086
     exit $1
 }
 
-trap cleanup EXIT
-
-# Running all the bats-tests in a dir with spaces:
-TEST_RUN_DIR="${SECRET_PROJECT_ROOT:?}/tempdir with spaces"
 TEST_TMP_DIR="${TMPDIR:=/tmp}/git-secret-bats-tmp"
-
-rm -rf "${TEST_RUN_DIR}" "${TEST_TMP_DIR}"
-mkdir -p "${TEST_RUN_DIR}" "${TEST_TMP_DIR}"
-cd "${TEST_RUN_DIR}"
+rm -rf "${TEST_TMP_DIR}"
+mkdir -p "${TEST_TMP_DIR}"
+cd "${SECRET_PROJECT_ROOT}"
 
 # test with non-standard SECRETS_DIR (normally .gitsecret) and SECRETS_EXTENSION (normally .secret)
 export SECRETS_DIR=.gitsecret-testdir
@@ -28,5 +26,5 @@ export SECRETS_EXTENSION=.secret2
 # bats expects diagnostic lines to be sent to fd 3, matching regex '^ #' (IE, like: `echo '# message here' >&3`)
 # bats ... 3>&1 shows diagnostic output when errors occur.
 TMPDIR="${TEST_TMP_DIR}" bats "${SECRET_PROJECT_ROOT}/tests/" 3>&1
-status=$?
-cleanup $status
+result=$?
+cleanup $result
