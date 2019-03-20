1aa6a199a9fe7ffc63c0ea21e5efb024a7c089ea
Josh Rabinowitz <joshr@joshr.com>
2019-03-19 21:31:31 -0400

    reset TMPDIR

--- utils/tests.sh
+++ utils/tests.sh
@@ -4,7 +4,8 @@
 
 set -e
 
-cd "${SECRET_PROJECT_ROOT}"; rm -rf 'tempdir'; mkdir 'tempdir'; cd 'tempdir'
+TMP_SUBDIR="tempdir"
+cd "${SECRET_PROJECT_ROOT}"; rm -rf "$TMP_SUBDIR"; mkdir "$TMP_SUBDIR"; cd "$TMP_SUBDIR"
 
 
 # test with non-standard SECRETS_DIR (normally .gitsecret) and SECRETS_EXTENSION (normally .secret)
@@ -12,8 +13,12 @@ export SECRETS_DIR=.gitsecret-testdir
 export SECRETS_EXTENSION=.secret2
 #export SECRETS_VERBOSE=''
 
+export TMPDIR="${SECRET_PROJECT_ROOT}/${TMP_SUBDIR}"
+echo "# TMPDIR is $TMPDIR" 
+
 # bats expects diagnostic lines to be sent to fd 3, matching regex '^ #' (IE, like: `echo '# message here' >&3`)
 # bats ... 3>&1 shows diagnostic output when errors occur.
 bats "${SECRET_PROJECT_ROOT}/tests/" 3>&1
 
-cd ..; rm -rf 'tempdir'
+cd ..; rm -rf "$TMPDIR"
+trap 'echo "# git-secret: cleaning up $TMPDIR" >&3; rm -rf "$TMPDIR";' EXIT
