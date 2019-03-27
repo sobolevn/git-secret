fc85d05fc05f97d9847830d64bb097929774ba90
Josh Rabinowitz <joshr@joshr.com>
2019-03-27 01:10:07 -0400

    Use /tmp/subdir for testing, not /tmp. (#442)

--- utils/tests.sh
+++ utils/tests.sh
@@ -4,16 +4,26 @@
 
 set -e
 
-# Running all the bats-tests in a dir with spaces:
-cd "${SECRET_PROJECT_ROOT}"; rm -rf 'tempdir with spaces'; mkdir 'tempdir with spaces'; cd 'tempdir with spaces';
+#TEST_DIR="/tmp/tempdir with spaces"
+TEST_DIR="/tmp/tempdir"
 
-# test with non-standard SECRETS_DIR (normally .gitsecret) and SECRETS_EXTENSION (normally .secret)
-export SECRETS_DIR=.gitsecret-testdir
-export SECRETS_EXTENSION=.secret2
-#export SECRETS_VERBOSE=''
+rm -rf "${TEST_DIR}" 
+mkdir "${TEST_DIR}"
+chmod 0700 "${TEST_DIR}"
+(
+    cd "${TEST_DIR}"
 
-# bats expects diagnostic lines to be sent to fd 3, matching regex '^ #' (IE, like: `echo '# message here' >&3`)
-# bats ... 3>&1 shows diagnostic output when errors occur.
-bats "${SECRET_PROJECT_ROOT}/tests/" 3>&1
+    # test with non-standard SECRETS_DIR (normally .gitsecret) and SECRETS_EXTENSION (normally .secret)
+    export SECRETS_DIR=.gitsecret-testdir
+    export SECRETS_EXTENSION=.secret2
+    #export SECRETS_VERBOSE=''
 
-cd ..; rm -rf 'tempdir with spaces'
+    #export TMPDIR="${TEST_DIR}"
+    #echo "# TMPDIR is $TMPDIR" 
+
+    # bats expects diagnostic lines to be sent to fd 3, matching regex '^ #' (IE, like: `echo '# message here' >&3`)
+    # bats ... 3>&1 shows diagnostic output when errors occur.
+    bats "${SECRET_PROJECT_ROOT}/tests/" 3>&1
+)
+
+rm -rf "${TEST_DIR}"
