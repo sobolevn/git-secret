bbb4d9d3e5dbaeef07faf9a6ec946435a3e3bff1
Josh Rabinowitz <joshr@joshr.com>
2019-04-01 12:53:19 -0400

    Tmp cleanups (#457)
    * Only show 'cleaning up temp file' messages if one
      of SECRETS_VERBOSE or SECRETS_TEST_VERBOSE is set.
    * Adjust tests to reflect the change.
    * Add note about suppressing 'cleaning up' msgs to CHANGELOG.md and reorder entries
    * add comments showing examples of tmpfiles returned

--- src/_utils/_git_secret_tools_linux.sh
+++ src/_utils/_git_secret_tools_linux.sh
@@ -10,7 +10,7 @@ function __replace_in_file_linux {
 function __temp_file_linux {
   : "${TMPDIR:=/tmp}"
   local filename
-  filename=$(mktemp -t _git_secret_XXX )
+  filename=$(mktemp -t _git_secret_XXX )    # makes a filename like /tmp/_git_secret_6Hw
   echo "$filename"
 }
 
