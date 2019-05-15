bd142901a333e6c323d4e5059f5c1545ed5c6837
Josh Rabinowitz <joshr@joshr.com>
2019-05-15 09:13:17 -0400

    change mktemp template filename

--- tests/_test_base.bash
+++ tests/_test_base.bash
@@ -13,7 +13,7 @@ FIXTURES_DIR="$BATS_TEST_DIRNAME/fixtures"
 
 TEST_GPG_HOMEDIR="$BATS_TMPDIR"
 
-TEST_GPG_OUTPUT_FILE=$(TMPDIR="$BATS_TMPDIR" mktemp -t 'gitsecret_output_XXXXXX')
+TEST_GPG_OUTPUT_FILE=$(TMPDIR="$BATS_TMPDIR" mktemp -t 'gitsecret_output.XXXXXX')
 echo "# TEST_GPG_OUTPUT_FILE=$TEST_GPG_OUTPUT_FILE" >&3
 
 # shellcheck disable=SC2016
