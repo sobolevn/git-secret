8c8a694ae0455e5a53aacc45e08d907f473b1c5f
Josh Rabinowitz <joshr@joshr.com>
2019-10-02 11:53:13 -0400

    improve comments

--- src/_utils/_git_secret_tools.sh
+++ src/_utils/_git_secret_tools.sh
@@ -657,20 +657,22 @@ function _get_users_in_gpg_keyring {
   fi
 
   # We use --fixed-list-mode so older versions of gpg emit 'uid:' lines.
-  # Gawk splits on colon as --with-colon, exact matches field 1 as 'uid', and selects field 10 "User-ID" 
-  #  and checks $2 is not i=invalid, d=disabled, r=revoked, e=expired, n=not valid; for #508 / #552:
-  # See https://github.com/gpg/gnupg/blob/master/doc/DETAILS#field-2---validity # for more on gpg 'validity codes'.
-  # The gensub regex extracts email from <> within field 10. (If there's no <>, then field is just an email address 
-  #  (and maybe a comment) and the regex just passes it through.)
-  # Sed at the end removes any 'comment' that appears in parentheses, for #530
+  # Gawk splits on colon as --with-colon, matches field 1 as 'uid', 
   # 3>&- closes fd 3 for bats, see https://github.com/bats-core/bats-core#file-descriptor-3-read-this-if-bats-hangs
   result=$($SECRETS_GPG_COMMAND "${args[@]}" --no-permission-warning --list-public-keys --with-colon --fixed-list-mode | \
-      gawk -F: '$1=="uid"')
+      gawk -F: '$1~/uid/' 3>&-)
+      #gawk -F: '$1=="uid"')
 
-  if [[ "$active_only" -ne 0 ]]; then
+  # For #508 / #552: if user asked for only active keys, check $2 is none of:
+  # i=invalid, d=disabled, r=revoked, e=expired, n=not valid
+  # See https://github.com/gpg/gnupg/blob/master/doc/DETAILS#field-2---validity # for more on gpg 'validity codes'.
+  if [[ "$active_only" -eq 1 ]]; then
       result=$(echo "$result" | gawk -F: '$2!="i" && $2!="d" && $2!="r" && $2!="e" && $2!="n"')
   fi
 
+  # gensub() outputs email from <> within field 10, "User-ID".  If there's no <>, then field is just an email address 
+  #  (and maybe a comment) and we pass it through.
+  # Sed at the end removes any 'comment' that appears in parentheses, for #530
   result=$(echo "$result" | gawk -F: '{print gensub(/.*<(.*)>.*/, "\\1", "g", $10); }' | sed 's/([^)]*)//g' )
 
   echo "$result"
