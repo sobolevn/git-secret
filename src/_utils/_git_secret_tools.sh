727b1835c36f8d65a1c606dcc38587554d57908f
Josh Rabinowitz <joshr@joshr.com>
2019-10-05 17:06:14 -0400

    fix

--- src/_utils/_git_secret_tools.sh
+++ src/_utils/_git_secret_tools.sh
@@ -594,7 +594,7 @@ function _get_user_key_expiry {
   secrets_dir_keys=$(_get_secrets_dir_keys)
 
   # 3>&- closes fd 3 for bats, see https://github.com/bats-core/bats-core#file-descriptor-3-read-this-if-bats-hangs
-  (set "$SECRETS_SET_FLAG"; line=$($SECRETS_GPG_COMMAND --homedir "$secrets_dir_keys" --no-permission-warning --list-public-keys --with-colon --fixed-list-mode "$username" | grep ^pub: 3>&-))
+  line=$($SECRETS_GPG_COMMAND --homedir "$secrets_dir_keys" --no-permission-warning --list-public-keys --with-colon --fixed-list-mode "$username" | grep ^pub: 3>&-)
 
   local expiry_epoch
   expiry_epoch=$(echo "$line" | cut -d: -f7)
