282c36d5eb74b421ddff29740de7778798b39a91
Elisei Roca <eroca@suse.de>
2020-10-02 22:02:33 +0200

    make "usage" message easier to read (#617)

--- src/commands/git_secret_usage.sh
+++ src/commands/git_secret_usage.sh
@@ -15,25 +15,24 @@ function usage {
   shift $((OPTIND-1))
   [ "$1" = "--" ] && shift
 
-  # There was a bug with some shells, which were adding extra commands
-  # to the old dynamic-loading version of this code.
-  # thanks to @antmak it is now fixed, see:
-  # https://github.com/sobolevn/git-secret/issues/47
-  local commands="add|cat|changes|clean|hide|init|killperson|list|remove|reveal|tell|usage|whoknows"
-
-  echo "usage: git secret [--version] [$commands]"
-  echo " 'git secret --version' will show version and exit"
-  echo "See 'git secret [command] -h' for more info about commands and their options"
-  echo " add [filename.txt] - adds file to be hidden, optionally adds file to .gitignore"
-  echo " cat [filename.txt] - cats the decrypted contents of the named file to stdout"
-  echo " changes [filename.secret] - indicates if the file changed since last commit"
-  echo " clean - deletes encrypted files"
-  echo " hide - encrypts (or re-encrypts) the files to be hidden"
-  echo " init - creates the .gitsecret directory and contents needed for git-secret"
-  echo " killperson [emails] - the reverse of 'tell', removes access for the named user"
-  echo " list - shows files to be hidden/encrypted, as in .gitsecret/paths/mapping.cfg"
-  echo " remove [files] - removes files from list of hidden files"
-  echo " reveal - decrypts all hidden files, as mentioned in 'git secret list'"
-  echo " tell [email] - add access for the user with imported public key with email"
-  echo " whoknows - shows list of email addresses associated with public keys that can reveal files"
+  echo "usage: git secret [--version] [command] [command-options]"
+  echo ""
+  echo "options:"
+  echo " --version                 - prints the version number"
+  echo ""
+  echo "commands:"
+  echo "see 'git secret [command] -h' for more info about commands and their options"
+  echo " add [file.txt]            - adds file to be hidden to the list"
+  echo " cat [file.txt]            - decrypts and prints contents of the file"
+  echo " changes [file.txt.secret] - indicates if the file changed since last commit"
+  echo " clean                     - deletes all encrypted files"
+  echo " hide                      - encrypts (or re-encrypts) the files to be hidden"
+  echo " init                      - initializes the  git-secret repository"
+  echo " killperson [emails]       - deletes a person's public key from the keyring"
+  echo " list                      - prints all the added files"
+  echo " remove [files]            - removes files from the list of hidden files"
+  echo " reveal                    - decrypts all hidden files"
+  echo " tell [email]              - imports a person's public key into the keyring"
+  echo " usage                     - prints this message"
+  echo " whoknows                  - prints list of authorized email addresses"
 }
