6fe9aeb858fe6b23e5026f25ef6ca4a8883603f6
sobolevn <mail@sobolevn.me>
2021-05-04 21:48:59 +0300

    Adds new installation instructions for deb and rpm

--- utils/deb/build.sh
+++ utils/deb/build.sh
@@ -3,12 +3,12 @@
 set -e
 
 # shellcheck disable=SC1090,SC1091
-source "${SECRET_PROJECT_ROOT}/utils/build-utils.sh"
+source "$SECRET_PROJECT_ROOT/utils/build-utils.sh"
 
-preinstall_files '-T'
+preinstall_files '-c'
 
 # Building .deb package:
 cd "$SCRIPT_DEST_DIR" && build_package 'deb'
 
 # Cleaning up:
-clean_up_files && cd "${SECRET_PROJECT_ROOT}"
+clean_up_files && cd "$SECRET_PROJECT_ROOT"
---
+++ utils/deb/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$SECRETS_ARTIFACTORY_CREDENTIALS" ]; then
+  echo '$SECRETS_ARTIFACTORY_CREDENTIALS is not set'
+  exit 1
+fi
+
+# shellcheck disable=SC1090,SC1091
+source "$SECRET_PROJECT_ROOT/utils/build-utils.sh"
+
+# Artifactory location:
+BASE_API_URL='https://gitsecret.jfrog.io/artifactory'
+
+# This folder should contain just one `.dev` file:
+DEB_FILE_LOCATION="$(locate_release 'deb')"
+DEB_FILE_NAME="$(basename "$DEB_FILE_LOCATION")"
+
+
+curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+  -XPUT "$BASE_API_URL/git-secret-deb/$DEB_FILE_NAME;deb.distribution=git-secret;deb.component=main;deb.architecture=all" \
+  -T "$DEB_FILE_LOCATION"
+
+# Now, we need to trigger metadata reindex:
+curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+  -XPOST "$BASE_API_URL/api/deb/reindex/git-secret-deb"
+
+echo
+echo "Done: released $DEB_FILE_NAME"
