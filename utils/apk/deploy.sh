37a28389d2ab2e2e7f3dd4eee4907964dee8b445
Nikita Sobolev <mail@sobolevn.me>
2021-05-04 21:58:36 +0300

    Adds new installation instructions for deb and rpm (#662)
    * Adds new installation instructions for deb and rpm
    
    * Fixes CI

---
+++ utils/apk/deploy.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$SECRETS_ARTIFACTORY_CREDENTIALS" ]; then
+  echo '$SECRETS_ARTIFACTORY_CREDENTIALS is not set'
+  exit 1
+fi
+
+# shellcheck disable=SC1090,SC1091
+source "$SECRET_PROJECT_ROOT/utils/build-utils.sh"
+# shellcheck disable=SC1090,SC1091
+source "$SECRET_PROJECT_ROOT/src/version.sh"
+
+VERSION_NAME="git-secret-${GITSECRET_VERSION}.apk"
+
+# Artifactory location:
+BASE_API_URL='https://gitsecret.jfrog.io/artifactory'
+
+# This folder should contain just one `.apk` file:
+APK_FILE_LOCATION="$(locate_release 'apk')"
+APK_FILE_NAME="$(basename "$APK_FILE_LOCATION")"
+
+
+function upload_with_architecture {
+  local arch="$1"
+
+  curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+    --max-time 10 \
+    --retry 3 \
+    --retry-delay 5 \
+    -XPUT "$BASE_API_URL/git-secret-apk/all/main/$arch/$VERSION_NAME" \
+    -T "$APK_FILE_LOCATION"
+}
+
+# Full list is here:
+# http://dl-cdn.alpinelinux.org/alpine/v3.13/main/
+ARCHITECTURES=(
+  'aarch64'
+  'armhf'
+  'armv7'
+  'mips64'
+  'ppc64le'
+  's390x'
+  'x86_64'
+  'x86'
+)
+
+for architecture in "${ARCHITECTURES[@]}"; do
+  upload_with_architecture "$architecture"
+done
+
+# Now, we need to trigger metadata reindex:
+curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+  --max-time 5 \
+  --retry 3 \
+  --retry-delay 5 \
+  -XPOST "$BASE_API_URL/api/alpine/git-secret-apk/reindex"
+
+echo
+echo "Done: released $APK_FILE_NAME"
---
+++ utils/rpm/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$SECRETS_ARTIFACTORY_CREDENTIALS" ]; then
+  echo '$SECRETS_ARTIFACTORY_CREDENTIALS is not set'
+  exit 1
+fi
+
+# shellcheck disable=SC1090,SC1091
+source "$SECRET_PROJECT_ROOT/utils/build-utils.sh"
+
+# Artifactory location:
+BASE_API_URL='https://gitsecret.jfrog.io/artifactory'
+
+# This folder should contain just one `.rpm` file:
+RPM_FILE_LOCATION="$(locate_release 'rpm')"
+RPM_FILE_NAME="$(basename "$RPM_FILE_LOCATION")"
+
+
+curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+  -XPUT "$BASE_API_URL/git-secret-rpm/rpm/$RPM_FILE_NAME" \
+  -T "$RPM_FILE_LOCATION"
+
+# Now, we need to trigger metadata reindex:
+curl -sS -u "$SECRETS_ARTIFACTORY_CREDENTIALS" \
+  -XPOST "$BASE_API_URL/api/yum/git-secret-rpm?async=1"
+
+echo
+echo "Done: released $RPM_FILE_NAME"
