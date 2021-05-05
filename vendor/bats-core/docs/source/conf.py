9c501e7229c39ea69c5f82640b8350d783769468
sobolevn <sobolevn@users.noreply.github.com>
2021-05-05 00:05:36 +0000

    Fixes by misspell-fixer

--- vendor/bats-core/docs/source/conf.py
+++ vendor/bats-core/docs/source/conf.py
@@ -32,7 +32,7 @@ release = '1'
 # ones.
 extensions = [
     'recommonmark',
-    'sphinxcontrib.programoutput'
+    'sphinxcontrib.programountput'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
