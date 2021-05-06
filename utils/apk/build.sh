79867c2ab76bbb7cf3e1d0dc0e48fe3f8a07cc4c
sobolevn <mail@sobolevn.me>
2021-05-06 17:47:41 +0300

    Version 0.4.0 release

--- utils/apk/build.sh
+++ utils/apk/build.sh
@@ -7,18 +7,32 @@ source "$SECRETS_PROJECT_ROOT/utils/build-utils.sh"
 # We need this export for `config.yml` to expand the version properly.
 export SCRIPT_VERSION
 
+# shellcheck disable=SC1090,SC1091
+source "$SECRETS_PROJECT_ROOT/utils/apk/meta.sh"
+
 # Create dest dir:
 mkdir -p "$SCRIPT_DEST_DIR"
 
-# Here's the deal. We use a custom builder here,
-# because `fpm` produces a broken package.
-# It is possible to install it locally,
-# but it fails to be installed from our Artifactory.
-# So, we switched.
-# But, we only switched one repo for now,
-# because we are not sure that other affected packages will be fine.
-# And also `fpm` supports more tools: like pacman.
-nfpm package \
-  -f "$SECRETS_PROJECT_ROOT/utils/apk/config.yml" \
-  --packager 'apk' \
-  --target "$SCRIPT_DEST_DIR"
+for architecture in "${ALPINE_ARCHITECTURES[@]}"; do
+  ALPINE_ARCHITECTURE="$architecture"
+  export ALPINE_ARCHITECTURE
+
+  config_file="$SCRIPT_BUILD_DIR/${architecture}.yml"
+  envsubst < "$SECRETS_PROJECT_ROOT/utils/apk/nfpm.yml" > "$config_file"
+
+  # Here's the deal. We use a custom builder here,
+  # because `fpm` produces a broken package.
+  # It is possible to install it locally,
+  # but it used to fail when installed from our Artifactory.
+  # So, we switched to `nfpm` instead.
+  # But, we only switched one repo for now,
+  # because we are not sure that other affected packages will be fine.
+  # And also `fpm` supports more tools: like pacman.
+  nfpm package \
+    --config "$config_file" \
+    --packager 'apk' \
+    --target "$SCRIPT_DEST_DIR"
+done
+
+# Cleaning up:
+clean_up_files
--- utils/rpm/install.sh
+++ utils/rpm/install.sh
@@ -2,5 +2,6 @@ wget https://raw.githubusercontent.com/sobolevn/git-secret/master/utils/rpm/git-
 # Inspect what's inside! You can also enable `gpg` check on repo level.
 sudo mv git-secret-rpm.repo /etc/yum.repos.d/
 sudo yum install -y git-secret
+
 # Testing, that it worked:
 git secret --version
