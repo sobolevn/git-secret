01f80ba15a1e7498c8155920eb72af3cd595c042
joshr <joshr>
2022-01-15 14:19:24 -0500

    restore whitespace

--- src/commands/git_secret_cat.sh
+++ src/commands/git_secret_cat.sh
@@ -33,6 +33,7 @@ function cat {
 
     filename=$(_get_record_filename "$line")
     path=$(_append_relative_root_path "$filename")  # this uses the _relative version because of #710
+
     # The parameters are: filename, write-to-file, force, homedir, passphrase
     _decrypt "$path" "0" "0" "$homedir" "$passphrase"
   done
