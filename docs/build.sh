74de74bde4b103c1ab5a56ee103cf397583cc76a
joshr <joshr>
2022-02-20 10:08:14 -0500

    quote directory name as per shellcheck

--- docs/build.sh
+++ docs/build.sh
@@ -29,7 +29,7 @@ function copy_to_posts {
   current_date=$(date "+%Y-%m-%d")
 
   # Creating command refernce:
-  for com in $MAN_LOCATION/git-secret-*.1.md; do
+  for com in "$MAN_LOCATION"/git-secret-*.1.md; do
     local short_name
     short_name=$(echo "$com" | sed -n "s|$MAN_LOCATION/\(.*\)\.1\.md|\1|p")
     local command_header="---
