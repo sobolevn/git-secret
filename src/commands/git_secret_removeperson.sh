a784b47b4522612aed3e7e7b0ce4233c6548d896
joshr <joshr>
2022-03-31 18:18:32 -0400

    fix mistake

--- src/commands/git_secret_removeperson.sh
+++ src/commands/git_secret_removeperson.sh
@@ -37,7 +37,7 @@ function removeperson {
 
   for email in "${emails[@]}"; do
     # see https://github.com/bats-core/bats-core#file-descriptor-3-read-this-if-bats-hangs for info about 3>&-
-    $SECRETS_GPG_COMMAND "${args[@]}" "$email" 3>&-
+    $SECRETS_GPG_COMMAND "${args[@]}" --delete-key "$email" 3>&-
     local exit_code=$?
     if [[ "$exit_code" -ne 0 ]]; then
       _abort "problem deleting key for '$email' with gpg: exit code $exit_code"
