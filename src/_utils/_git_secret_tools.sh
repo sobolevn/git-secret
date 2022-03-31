7fd1a554a4cf4a0969c3a2fb87514cdd2468b1b5
joshr <joshr>
2022-03-31 19:49:51 -0400

    shellcheck disables

--- src/_utils/_git_secret_tools.sh
+++ src/_utils/_git_secret_tools.sh
@@ -14,10 +14,14 @@ _SECRETS_DIR_KEYS_TRUSTDB="${_SECRETS_DIR_KEYS}/trustdb.gpg"
 _SECRETS_DIR_PATHS_MAPPING="${_SECRETS_DIR_PATHS}/mapping.cfg"
 
 function is_git_version_ge_2_28_0() { # based on code from github autopilot
+    # shellcheck disable=SC2155
     local git_version=$(git --version | awk '{print $3}')
-    local git_version_major=$(echo $git_version | awk -F. '{print $1}')
-    local git_version_minor=$(echo $git_version | awk -F. '{print $2}')
-    local git_version_patch=$(echo $git_version | awk -F. '{print $3}')
+    # shellcheck disable=SC2155
+    local git_version_major=$(echo "$git_version" | awk -F. '{print $1}')
+    # shellcheck disable=SC2155
+    local git_version_minor=$(echo "$git_version" | awk -F. '{print $2}')
+    # shellcheck disable=SC2155
+    local git_version_patch=$(echo "$git_version" | awk -F. '{print $3}')
     if [[ $git_version_major -ge 2 ]] && [[ $git_version_minor -ge 28 ]] && [[ $git_version_patch -ge 0 ]]; then
         return 0
     else
