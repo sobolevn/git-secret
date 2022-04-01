50ec69d07a7fa177009aff5944fb4eda276bdce7
joshr <joshr>
2022-03-31 20:19:08 -0400

    improve output, and remove unused code

--- src/commands/git_secret_add.sh
+++ src/commands/git_secret_add.sh
@@ -2,12 +2,11 @@
 
 
 function add {
-  local auto_ignore=1
   OPTIND=1
 
   while getopts "ihv" opt; do
     case "$opt" in
-      i) auto_ignore=1;;    # this doesn't change anything
+      i) ;;    # this doesn't change anything
 
       h) _show_manual_for "add";;
 
@@ -59,26 +58,14 @@ function add {
   # Are there any unignored files?
 
   if [[ ! "${#not_ignored[@]}" -eq 0 ]]; then
-    # And show them all at once.
-    local message
-    message="these files are not in .gitignore: ${not_ignored[*]}"
-    # NOTE: message has spaces between filenames, which can be ambiguous if files also have spaces
+    # Add these files to `.gitignore` automatically:
+    # see https://github.com/sobolevn/git-secret/issues/18 for more.
 
-    if [[ "$auto_ignore" -eq 0 ]]; then
-      # This file is not ignored. user don't want it to be added automatically.
-      # Raise the exception, since all files, which will be hidden, must be ignored.
-      # note that it is no longer possible to wind up in this code path as auto_ignore cannot be 0.
-      # code left here in case we want to restore/modify this path later
-      _abort "$message"
-    else
-      # In this case these files should be added to the `.gitignore` automatically:
-      # see https://github.com/sobolevn/git-secret/issues/18 for more.
-      _message "$message"
-      _message "auto adding them to .gitignore"
-      for item in "${not_ignored[@]}"; do
-        _add_ignored_file "$item"
-      done
-    fi
+    # NOTE: output has spaces between filenames, which can be ambiguous if files also have spaces
+    _message "files not in .gitignore, adding: ${not_ignored[*]}"
+    for item in "${not_ignored[@]}"; do
+      _add_ignored_file "$item"
+    done
   fi
 
   # Adding files to path mappings:
