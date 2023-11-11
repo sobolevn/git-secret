d8c9b223c951a2f645ee5b2ba7782489e73e7b13
Dirk Elmendorf <dirk@r26d.com>
2023-11-11 06:39:12 -0600

    Extra Line
    Removing line that was duplicated.

--- utils/deb/install.sh
+++ utils/deb/install.sh
@@ -2,7 +2,7 @@ wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | gpg --d
 sudo apt-get install apt-transport-https  ca-certificates --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/git-secret.gpg] https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main" | sudo tee /etc/apt/sources.list.d/git-secret.list
 sudo apt-get update && sudo apt-get install -y git-secret
-sudo apt-get update && sudo apt-get install -y git-secret
+
 
 # Testing, that it worked:
 git secret --version
